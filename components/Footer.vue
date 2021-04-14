0e86d53da471cbcf8577ff32371bdda749cdea2b
csengmany <sengmany.cathy@gmail.com>
2021-04-14 12:28:58 +0200

    initial

---
+++ components/Footer.vue
@@ -0,0 +1,70 @@
+<template>
+  <footer>
+    <span>
+        <a href="mailto:cathy.sengmany.pro@gmail.com">
+            <FontAwesomeIcon
+                        icon={faEnvelopeSquare}
+                        size="2x"
+                        className="footer-icon"
+                    />
+        </a>
+        <a
+                    href="https://github.com/csengmany"
+                    target="_blank"
+                    rel="noreferrer"
+        >
+            <FontAwesomeIcon
+                        icon={faGithubSquare}
+                        size="2x"
+                        className="footer-icon"
+                    />
+        </a>
+                <a
+                    href="https://www.linkedin.com/in/cathy-sengmany/"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <FontAwesomeIcon
+                        icon={faLinkedin}
+                        size="2x"
+                        className="footer-icon"
+                    />
+                </a>
+            </span>
+            <span>
+                © {{date}}&nbsp;Cathy Sengmany, built with <Logo class="footer-logo"/>
+            </span>
+  </footer>
+</template>
+
+<script>
+export default {
+    data(){
+        return{date:new Date().getFullYear()}
+    }
+
+}
+</script>
+
+<style>
+
+footer{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.footer-logo{
+    height: 20px;
+    width: 25px;
+
+}
+.footer-logo:hover{
+    transition-duration:0.5s;
+    transform: translateX(20px);
+    transform: scale(1.2);
+}
+
+
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="container">
+    <div class="content-wrap">
+      <NewLogo id="logo"/>
+      <h1 class="title">
+        Useless Knowledge 
+      </h1>
+      <h2>Today we are the {{displayDate(today)}} and the sentence of the moment is : </h2>
+      <div class="frame" @click="copyText()">
+        <span v-if="seen">Copy !</span>
+        <h3  >{{sentence}}</h3>
+      </div>
+      <button @click="update()">Reroll a new sentence</button>
+    </div>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import axios from"axios";
+import "../styles/index.css"
+import "@/styles/app.css"
+export default {
+  data(){
+    return{ sentence:"helooo",
+    today : new Date(),
+    seen:false
+    }
+  }, mounted(){
+    axios.get("https://uselessfacts.jsph.pl/random.json?language=en").then(response => this.sentence =response.data.text)
+  },
+  methods:{
+   displayDate(date){
+  const dayNumber = date.getDate();
+  const day = date.getDay(); // number between 0 and 6
+  const month = date.getMonth(); // number between 0 and 11
+  const year = date.getFullYear();
+
+  let dayName;
+  let monthName;
+
+  switch (day) {
+    case 0:
+      dayName = "Sunday";
+      break;
+    case 1:
+      dayName = "Monday";
+      break;
+    case 2:
+      dayName = "Tuesday";
+      break;
+    case 3:
+      dayName = "Wednesday";
+      break;
+    case 4:
+      dayName = "Thurdsday";
+      break;
+    case 5:
+      dayName = "Friday";
+      break;
+    case 6:
+      dayName = "Saturday";
+      break;
+  }
+
+  switch (month) {
+    case 0:
+      monthName = "January";
+      break;
+    case 1:
+      monthName = "February";
+      break;
+    case 2:
+      monthName = "March";
+      break;
+    case 3:
+      monthName = "April";
+      break;
+    case 4:
+      monthName = "May";
+      break;
+    case 5:
+      monthName = "June";
+      break;
+    case 6:
+      monthName = "July";
+      break;
+    case 7:
+      monthName = "August";
+      break;
+    case 8:
+      monthName = "September";
+      break;
+    case 9:
+      monthName = "October";
+      break;
+    case 10:
+      monthName = "November";
+      break;
+    case 11:
+      monthName = "December";
+      break;
+      }
+const sentence = dayName +" "+ dayNumber +" of "+monthName + ", "+year
+  return sentence;
+    },
+    update(){
+          axios.get("https://uselessfacts.jsph.pl/random.json?language=en").then(response => this.sentence =response.data.text)
+
+    },
+    copyText () {
+         navigator.clipboard.writeText(this.sentence);
+         this.seen=true;
+         setTimeout(() => this.seen = false, 2000)
+        }
+        
+  }
+}
+</script>
+
